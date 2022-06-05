bd2301a1eb9f0091f19169ae9c345985c93f289c
João Vitor Altermann Martinelli <76072847+joaomarti755@users.noreply.github.com>
2022-06-04 23:02:45 -0300

    Tentativa de Teste na Questão02

--- TrabalhoOrientacaoObjetos01/TrabalhoOrientacaoObjetos01.Tests/Questao02/CalendarioTests.cs
+++ TrabalhoOrientacaoObjetos01/TrabalhoOrientacaoObjetos01.Tests/Questao02/CalendarioTests.cs
@@ -1,20 +1,43 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrabalhoOrientacaoObjetos01.Questao02;
 using Xunit;
 
 namespace TrabalhoOrientacaoObjetos01.Tests.Questao02
 {
-    public class Calendario
+    public class CalendarioTests
     {
-        [Fact]
-        public void Validar_Dia()
-        {
-            //Arranger
-            //var calendario = new Calendario();
-            //calendario
-        }
+        //[Fact]
+        //public void Validar_Dia()
+        //{
+        //    //Arranger
+        //    var calendario = new Calendario();
+        //    calendario.Data.ToString().Substring(0, 2);
+
+        //    //Act
+        //    var diaPorExtenson = calendario.Dia();
+        //    var ValidarDiaPorExtenso = "Um";
+
+        //    //Assert
+        //    diaPorExtenson.Should().Be(ValidarDiaPorExtenso);
+        //}
+        //[Fact]
+        //public void Validar_Mes()
+        //{
+        //    //arranger
+        //    var calendario = new Calendario();
+        //    calendario.Data.ToString("MMMM");
+
+        //    //Act
+        //    var mesPorExtenso = calendario.Mes();
+        //    var validarMesPorExtenso = "janeiro";
+
+        //    //Assert
+        //    mesPorExtenso.Should().Be(validarMesPorExtenso);
+        //}
     }
 }
